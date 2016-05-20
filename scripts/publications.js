f732773e183df4ece8ecb1da351f470eac384065
BillMills <mills.wj@gmail.com>
2016-05-20 10:30:51 -0700

    publications stub

---
+++ scripts/publications.js
@@ -0,0 +1,15 @@
+publications = [
+    {
+        title: 'demo title 1',
+        authors: 'X, Y, Z',
+        link: 'http://example.com',
+        linkTitle: 'Phys Rev Z'
+    },
+
+    {
+        title: 'demo title 2',
+        authors: 'A, B, C',
+        link: 'http://example.com',
+        linkTitle: 'Phys Rev Z'
+    } 
+]
