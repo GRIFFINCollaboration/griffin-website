1bb7e48ac552debb0cd48fd21ca1aea21f6da152
BillMills <mills.wj@gmail.com>
2016-05-26 13:59:47 -0700

    added real pubs

--- scripts/publications.js
+++ scripts/publications.js
@@ -1,15 +1,22 @@
 publications = [
     {
-        title: 'demo title 1',
-        authors: 'X, Y, Z',
-        link: 'http://example.com',
-        linkTitle: 'Phys Rev Z'
+        title: 'Characteristics of GRIFFIN high-purity germanium clover detectors',
+        authors: 'U. Rizwan et al.',
+        link: 'http://www.sciencedirect.com/science/article/pii/S0168900216300341',
+        linkTitle: 'Nucl. Inst. Meths. A 820, 126 (2016)'
     },
 
     {
-        title: 'demo title 2',
-        authors: 'A, B, C',
-        link: 'http://example.com',
-        linkTitle: 'Phys Rev Z'
-    } 
+        title: 'Exploring exotic nuclei with the GRIFFIN Spectrometer at TRIUMF-ISAC',
+        authors: 'A.B. Garnsworthy',
+        link: 'http://www.actaphys.uj.edu.pl/fulltext?series=Reg&vol=47&page=713',
+        linkTitle: 'Acta Phys.Pol. B, 47, 713 (2016)'
+    },
+
+    {
+        title: 'The GRIFFIN Spectrometer',
+        authors: 'C.E. Svensson and A.B. Garnsworthy',
+        link: 'http://link.springer.com/article/10.1007/s10751-013-0889-3',
+        linkTitle: 'Hyperfine Interactions, 225, 127 (2014)'
+    }    
 ]
