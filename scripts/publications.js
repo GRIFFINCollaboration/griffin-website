82b4b58c5f924f4d894c1feea2a714f7fa1b2ac2
Adam Garnsworthy <garns@96-s84-v14-wlan.triumf.ca>
2016-05-31 13:31:53 -0700

    Changed format of publications table to Results, technical and conference proceedings

--- scripts/publications.js
+++ scripts/publications.js
@@ -1,16 +1,25 @@
-publications = [
+physics = [
     {
-        title: 'Characteristics of GRIFFIN high-purity germanium clover detectors',
-        authors: 'U. Rizwan et al.',
-        link: 'http://www.sciencedirect.com/science/article/pii/S0168900216300341',
-        linkTitle: 'Nucl. Inst. Meths. A 820, 126 (2016)'
-    },
-
+        title: 'Half-Lives of Neutron-Rich 128-130Cd',
+        authors: 'R. Dunlop',
+        link: 'http://journals.aps.org/prc/',
+        linkTitle: 'Submitted to Phys. Rev. C'
+    }
+]
+conference = [
     {
         title: 'Exploring exotic nuclei with the GRIFFIN Spectrometer at TRIUMF-ISAC',
         authors: 'A.B. Garnsworthy',
         link: 'http://www.actaphys.uj.edu.pl/fulltext?series=Reg&vol=47&page=713',
         linkTitle: 'Acta Phys.Pol. B, 47, 713 (2016)'
+    }  
+]
+technical = [
+    {
+        title: 'Characteristics of GRIFFIN high-purity germanium clover detectors',
+        authors: 'U. Rizwan et al.',
+        link: 'http://www.sciencedirect.com/science/article/pii/S0168900216300341',
+        linkTitle: 'Nucl. Inst. Meths. A 820, 126 (2016)'
     },
 
     {
