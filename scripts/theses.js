26de3e5ba2bf1b3c19d99b81122a3f4e994572a2
Adam Garnsworthy <garns@wlan2-162.triumf.ca>
2018-09-19 09:22:12 -0700

    Added a Thesis page

---
+++ scripts/theses.js
@@ -0,0 +1,68 @@
+phd = [
+    {
+        student: 'Michelle Dunlop',
+        instiution: 'University of Guelph',
+        supervisor: 'C.E. Svensson',
+        year: '2018',
+        link: 'https://www.physics.uoguelph.ca/Nucweb/theses/Dunlop_Michelle_201808_PhD.pdf',
+        linkTitle: 'High-Precision Half-Life Measurements for the Superallowed Fermi Beta+ Emitters 10C and 22Mg'
+    },
+    {
+        student: 'Jennifer Pore',
+        instiution: 'Simon Fraser University',
+        supervisor: 'C. Andreoiu',
+        year: '2016',
+        link: 'https://griffin.triumf.ca',
+        linkTitle: 'The Investigation of the Beta- Decay of 46K: Detailed Spectroscopy of the Low-Lying Structure of 46Ca with the GRIFFIN Spectrometer'
+    }
+]
+masters = [
+    {
+        student: 'Yukiya Saito',
+        instiution: 'University of British Columbia/TRIUMF',
+        supervisor: 'I. Dillmann',
+        year: '2018',
+        link: 'https://griffin.triumf.ca',
+        linkTitle: 'Decay spectroscopy of neutron-rich 129Cd with the GRIFFIN spectrometer'
+    },
+    {
+        student: 'Calum Jones',
+        instiution: 'University of Surrey/TRIUMF',
+        supervisor: 'A.B. Garnsworthy',
+        year: '2018',
+        link: 'https://griffin.triumf.ca',
+        linkTitle: 'An Investigation of Branching Ratios, Comparative Half-Lives and Rotational Band Structure in 50Ti Nuclei'
+    },
+    {
+        student: 'James Measures',
+        instiution: 'University of Surrey/TRIUMF',
+        supervisor: 'A.B. Garnsworthy',
+        year: '2017',
+        link: 'https://griffin.triumf.ca',
+        linkTitle: 'Structure of 26Mg Through the Analysis of the Beta Decay of 26Na'
+    },
+    {
+        student: 'Sam Hallam',
+        instiution: 'University of Surrey/TRIUMF',
+        supervisor: 'A.B. Garnsworthy',
+        year: '2016',
+        link: 'https://griffin.triumf.ca',
+        linkTitle: 'Designing CEDAR: Conversion Electron Detection ARray'
+    },
+    {
+        student: 'Usman Rizwan',
+        instiution: 'Simon Fraser University',
+        supervisor: 'K. Starosta',
+        year: '2015',
+        link: 'https://griffin.triumf.ca',
+        linkTitle: 'Development of Gamma-Ray Spectroscopy Techniques for Fundamental and Applied Research'
+    },
+    {
+        student: 'Lisa Morrison',
+        instiution: 'University of Surrey/TRIUMF',
+        supervisor: 'A.B. Garnsworthy',
+        year: '2015',
+        link: 'https://griffin.triumf.ca',
+        linkTitle: 'Gamma-Gamma Angular Correlations with the GRIFFIN Spectrometer'
+    }
+]
