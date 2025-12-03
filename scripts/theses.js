1764393fb87ef8416bffe8dd64bf9c451bfa9f89
AdamGarnsworthy <garnznation@gmail.com>
2025-12-03 11:17:46 -0800

    Added three masters thesis.

--- scripts/theses.js
+++ scripts/theses.js
@@ -1,220 +1,244 @@
 phd = [
-    {
-        student: 'Magda Satrazani',
-        institution: 'University of Liverpool',
-        supervisor: 'L. Gaffney',
-        year: '2024',
-        link: 'https://livrepository.liverpool.ac.uk/3181848/',
-        linkTitle: 'Gamma-ray spectroscopy of neutron-rich cerium isotopes following beta-decay of mass-separated caesium ions'
-    },
-    {
-        student: 'Connor Natzke',
-        institution: 'Colorado School of Mines/TRIUMF',
-        supervisor: 'K. Leach/A.B. Garnsworthy',
-        year: '2024',
-        link: 'https://repository.mines.edu/entities/publication/28fbfecf-f667-44c7-82d6-5471e8349285',
-        linkTitle: 'Search for nuclear two-photon decay with GRIFFIN'
-    },
-    {
-        student: 'Daniel Yates',
-        institution: 'University of British Columbia/TRIUMF',
-        supervisor: 'R. Kruecken',
-        year: '2024',
-        link: 'https://open.library.ubc.ca/soa/cIRcle/collections/ubctheses/24/items/1.0444092',
-        linkTitle: 'Advances in decay spectroscopy of 160Gd and developments for transfer experiments using radioactive isotope beams'
-    },
-    {
-        student: 'Desislava Kalaydjieva',
-        institution: 'Universit&#233; Paris-Saclay',
-        supervisor: 'M. Zieli&#324;ska/W. Korten',
-        year: '2023',
-        link: 'https://theses.hal.science/tel-04472302',
-        linkTitle: "Study of shape coexistence in nuclei with A&#8776;100 using stable and radioactive ion beams"
-    },
-    {
-        student: 'Kevin Ortner',
-        institution: 'Simon Fraser University',
-        supervisor: 'C. Andreoiu',
-        year: '2023',
-        link: 'http://summit.sfu.ca/item/36549',
-        linkTitle: 'Nuclear structure of 118Sn studied through beta decay of 118In and thermal neutron capture of 117Sn(n,gamma)'
-    },
-    {
-        student: 'Harris Bidaman',
-        institution: 'University of Guelph',
-        supervisor: 'P. Garrett',
-        year: '2023',
-        link: 'https://hdl.handle.net/10214/27505',
-        linkTitle: 'Lifetime Analysis of 100Zr and Simulating the Detector Array for Energy Measurements Of Neutrons (DAEMON)'
-    },
-    {
-        student: 'Carlotta Porzio',
-        institution: 'Universit&#224; degli Studi di Milano/TRIUMF',
-        supervisor: 'S. Leoni/A.B. Garnsworthy',
-        year: '2021',
-        link: 'https://air.unimi.it/handle/2434/886195',
-        linkTitle: 'Configuration mixing investigation in germanium isotopes'
-    },
-    {
-        student: 'Andrew MacLean',
-        institution: 'University of Guelph',
-        supervisor: 'C.E. Svensson',
-        year: '2021',
-        link: 'https://hdl.handle.net/10214/26382',
-        linkTitle: 'Spectroscopy of 188Hg following the beta+/EC decay of 188,188mTl'
-    },
-    {
-        student: 'Fatima Garcia',
-        institution: 'Simon Fraser University',
-        supervisor: 'C. Andreoiu',
-        year: '2021',
-        link: 'http://summit.sfu.ca/item/21551',
-        linkTitle: 'Spectroscopic studies of neutron-rich 129Sn and 80Ge nuclei using the GRIFFIN spectrometer'
-    },
-    {
-        student: 'Ryan Dunlop',
-        institution: 'University of Guelph',
-        supervisor: 'C.E. Svensson',
-        year: '2019',
-        link: 'http://hdl.handle.net/10214/16278',
-        linkTitle: 'Beta and beta-delayed neutron decay of the N = 82 nuclei 128-130Cd and 131In studied with the Gamma-Ray Infrastructure for Fundamental Investigations of Nuclei (GRIFFIN)'
-    },
-    {
-        student: 'Nikita Bernier',
-        institution: 'University of British Columbia/TRIUMF',
-        supervisor: 'R. Kruecken/I. Dillmann',
-        year: '2018',
-        link: 'https://open.library.ubc.ca/cIRcle/collections/ubctheses/24/items/1.0376019',
-        linkTitle: 'Decay Spectroscopy of Neutron-Rich Cadmium Around the N = 82 Shell Closure'
-    },
-    {
-        student: 'Michelle Dunlop',
-        institution: 'University of Guelph',
-        supervisor: 'C.E. Svensson',
-        year: '2018',
-        link: 'https://www.physics.uoguelph.ca/Nucweb/theses/Dunlop_Michelle_201808_PhD.pdf',
-        linkTitle: 'High-Precision Half-Life Measurements for the Superallowed Fermi Beta+ Emitters 10C and 22Mg'
-    },
-    {
-        student: 'Jennifer Pore',
-        institution: 'Simon Fraser University',
-        supervisor: 'C. Andreoiu',
-        year: '2016',
-        link: 'http://summit.sfu.ca/item/16967',
-        linkTitle: 'The Investigation of the Beta- Decay of 46K: Detailed Spectroscopy of the Low-Lying Structure of 46Ca with the GRIFFIN Spectrometer'
-    }
+  {
+    student: 'Magda Satrazani',
+    institution: 'University of Liverpool',
+    supervisor: 'L. Gaffney',
+    year: '2024',
+    link: 'https://livrepository.liverpool.ac.uk/3181848/',
+    linkTitle: 'Gamma-ray spectroscopy of neutron-rich cerium isotopes following beta-decay of mass-separated caesium ions'
+  },
+  {
+    student: 'Connor Natzke',
+    institution: 'Colorado School of Mines/TRIUMF',
+    supervisor: 'K. Leach/A.B. Garnsworthy',
+    year: '2024',
+    link: 'https://repository.mines.edu/entities/publication/28fbfecf-f667-44c7-82d6-5471e8349285',
+    linkTitle: 'Search for nuclear two-photon decay with GRIFFIN'
+  },
+  {
+    student: 'Daniel Yates',
+    institution: 'University of British Columbia/TRIUMF',
+    supervisor: 'R. Kruecken',
+    year: '2024',
+    link: 'https://open.library.ubc.ca/soa/cIRcle/collections/ubctheses/24/items/1.0444092',
+    linkTitle: 'Advances in decay spectroscopy of 160Gd and developments for transfer experiments using radioactive isotope beams'
+  },
+  {
+    student: 'Desislava Kalaydjieva',
+    institution: 'Universit&#233; Paris-Saclay',
+    supervisor: 'M. Zieli&#324;ska/W. Korten',
+    year: '2023',
+    link: 'https://theses.hal.science/tel-04472302',
+    linkTitle: "Study of shape coexistence in nuclei with A&#8776;100 using stable and radioactive ion beams"
+  },
+  {
+    student: 'Kevin Ortner',
+    institution: 'Simon Fraser University',
+    supervisor: 'C. Andreoiu',
+    year: '2023',
+    link: 'http://summit.sfu.ca/item/36549',
+    linkTitle: 'Nuclear structure of 118Sn studied through beta decay of 118In and thermal neutron capture of 117Sn(n,gamma)'
+  },
+  {
+    student: 'Harris Bidaman',
+    institution: 'University of Guelph',
+    supervisor: 'P. Garrett',
+    year: '2023',
+    link: 'https://hdl.handle.net/10214/27505',
+    linkTitle: 'Lifetime Analysis of 100Zr and Simulating the Detector Array for Energy Measurements Of Neutrons (DAEMON)'
+  },
+  {
+    student: 'Carlotta Porzio',
+    institution: 'Universit&#224; degli Studi di Milano/TRIUMF',
+    supervisor: 'S. Leoni/A.B. Garnsworthy',
+    year: '2021',
+    link: 'https://air.unimi.it/handle/2434/886195',
+    linkTitle: 'Configuration mixing investigation in germanium isotopes'
+  },
+  {
+    student: 'Andrew MacLean',
+    institution: 'University of Guelph',
+    supervisor: 'C.E. Svensson',
+    year: '2021',
+    link: 'https://hdl.handle.net/10214/26382',
+    linkTitle: 'Spectroscopy of 188Hg following the beta+/EC decay of 188,188mTl'
+  },
+  {
+    student: 'Fatima Garcia',
+    institution: 'Simon Fraser University',
+    supervisor: 'C. Andreoiu',
+    year: '2021',
+    link: 'http://summit.sfu.ca/item/21551',
+    linkTitle: 'Spectroscopic studies of neutron-rich 129Sn and 80Ge nuclei using the GRIFFIN spectrometer'
+  },
+  {
+    student: 'Ryan Dunlop',
+    institution: 'University of Guelph',
+    supervisor: 'C.E. Svensson',
+    year: '2019',
+    link: 'http://hdl.handle.net/10214/16278',
+    linkTitle: 'Beta and beta-delayed neutron decay of the N = 82 nuclei 128-130Cd and 131In studied with the Gamma-Ray Infrastructure for Fundamental Investigations of Nuclei (GRIFFIN)'
+  },
+  {
+    student: 'Nikita Bernier',
+    institution: 'University of British Columbia/TRIUMF',
+    supervisor: 'R. Kruecken/I. Dillmann',
+    year: '2018',
+    link: 'https://open.library.ubc.ca/cIRcle/collections/ubctheses/24/items/1.0376019',
+    linkTitle: 'Decay Spectroscopy of Neutron-Rich Cadmium Around the N = 82 Shell Closure'
+  },
+  {
+    student: 'Michelle Dunlop',
+    institution: 'University of Guelph',
+    supervisor: 'C.E. Svensson',
+    year: '2018',
+    link: 'https://www.physics.uoguelph.ca/Nucweb/theses/Dunlop_Michelle_201808_PhD.pdf',
+    linkTitle: 'High-Precision Half-Life Measurements for the Superallowed Fermi Beta+ Emitters 10C and 22Mg'
+  },
+  {
+    student: 'Jennifer Pore',
+    institution: 'Simon Fraser University',
+    supervisor: 'C. Andreoiu',
+    year: '2016',
+    link: 'http://summit.sfu.ca/item/16967',
+    linkTitle: 'The Investigation of the Beta- Decay of 46K: Detailed Spectroscopy of the Low-Lying Structure of 46Ca with the GRIFFIN Spectrometer'
+  }
 ]
 masters = [
-    {
-        student: 'William Royer',
-        institution: 'University of Surrey/TRIUMF',
-        supervisor: 'A.B. Garnsworthy',
-        year: '2023',
-        link: 'https://griffin.triumf.ca',
-        linkTitle: 'Research Towards Commissioning ARIES, the New Ancillary Beta Tagging Array for Decay Spectroscopy at TRIUMF'
-    },
-    {
-        student: 'Robin Coleman',
-        institution: 'University of Guelph',
-        supervisor: 'C.E. Svensson',
-        year: '2020',
-        link: 'http://hdl.handle.net/10214/17744',
-        linkTitle: 'Beta Decay of Neutron-Rich 52K'
-    },
-    {
-        student: 'Charlotte Paxman',
-        institution: 'University of Surrey/TRIUMF',
-        supervisor: 'A.B. Garnsworthy',
-        year: '2020',
-        link: 'https://griffin.triumf.ca',
-        linkTitle: 'Internal conversion coefficients and spin assignments in Hg isotopes'
-    },
-    {
-        student: 'Joseph McAfee',
-        institution: 'University of Surrey/TRIUMF',
-        supervisor: 'A.B. Garnsworthy',
-        year: '2020',
-        link: 'https://griffin.triumf.ca',
-        linkTitle: 'Developing the Perturbed Angular Correlation spectroscopy (PACs) technique for the GRIFFIN spectrometer'
-    },
-    {
-        student: 'Daniel Yates',
-        institution: 'University of British Columbia/TRIUMF',
-        supervisor: 'R. Kruecken/I. Dillmann',
-        year: '2019',
-        link: 'http://hdl.handle.net/2429/71312',
-        linkTitle: 'Decay spectroscopy of Europium-160'
-    },
-    {
-        student: 'Max Winokan',
-        institution: 'University of Surrey/TRIUMF',
-        supervisor: 'A.B. Garnsworthy',
-        year: '2019',
-        link: 'https://griffin.triumf.ca/Theses/Surrey-MPhys-Winokan.pdf',
-        linkTitle: 'A new beta-scintillator array with optimized geometry for the GRIFFIN spectrometer'
-    },
-    {
-        student: 'Tammy Zidar',
-        institution: 'University of Guelph',
-        supervisor: 'C.E. Svensson',
-        year: '2018',
-        link: 'http://hdl.handle.net/10214/14706',
-        linkTitle: 'Beta decay of neutron-rich 33Mg'
-    },
-    {
-        student: 'Yukiya Saito',
-        institution: 'University of British Columbia/TRIUMF',
-        supervisor: 'R. Kruecken/I. Dillmann',
-        year: '2018',
-        link: 'https://open.library.ubc.ca/cIRcle/collections/ubctheses/24/items/1.0371206',
-        linkTitle: 'Decay spectroscopy of neutron-rich 129Cd with the GRIFFIN spectrometer'
-    },
-    {
-        student: 'Calum Jones',
-        institution: 'University of Surrey/TRIUMF',
-        supervisor: 'A.B. Garnsworthy',
-        year: '2018',
-        link: 'https://griffin.triumf.ca/Theses/Surrey-MPhys-Jones.pdf',
-        linkTitle: 'An Investigation of Branching Ratios, Comparative Half-Lives and Rotational Band Structure in 50Ti Nuclei'
-    },
-    {
-        student: 'James Measures',
-        institution: 'University of Surrey/TRIUMF',
-        supervisor: 'A.B. Garnsworthy',
-        year: '2017',
-        link: 'https://griffin.triumf.ca',
-        linkTitle: 'Structure of 26Mg Through the Analysis of the Beta Decay of 26Na'
-    },
-    {
-        student: 'Andrew MacLean',
-        institution: 'University of Guelph',
-        supervisor: 'C.E. Svensson',
-        year: '2016',
-        link: 'https://www.physics.uoguelph.ca/Nucweb/theses/AMacleanMsc.pdf',
-        linkTitle: 'Gamma-gamma angular correlation measurements with GRIFFIN'
-    },
-    {
-        student: 'Sam Hallam',
-        institution: 'University of Surrey/TRIUMF',
-        supervisor: 'A.B. Garnsworthy',
-        year: '2016',
-        link: 'https://griffin.triumf.ca/Theses/Surrey-MPhys-Hallam.pdf',
-        linkTitle: 'Designing CEDAR: Conversion Electron Detection ARray'
-    },
-    {
-        student: 'Usman Rizwan',
-        institution: 'Simon Fraser University',
-        supervisor: 'K. Starosta',
-        year: '2015',
-        link: 'http://summit.sfu.ca/item/15733',
-        linkTitle: 'Development of Gamma-Ray Spectroscopy Techniques for Fundamental and Applied Research'
-    },
-    {
-        student: 'Lisa Morrison',
-        institution: 'University of Surrey/TRIUMF',
-        supervisor: 'A.B. Garnsworthy',
-        year: '2015',
-        link: 'https://griffin.triumf.ca/Theses/Surrey-MPhys-Morrison.pdf',
-        linkTitle: 'Gamma-Gamma Angular Correlations with the GRIFFIN Spectrometer'
-    }
+  {
+    student: 'Jizhong Liu',
+    institution: 'University of Regina',
+    supervisor: 'G.F. Grinyer',
+    year: '2024',
+    link: 'https://uregina.scholaris.ca/items/4418ccd5-4b11-47ee-b457-cb1e543b1074',
+    linkTitle: 'Beta-delayed proton emission of magnesium-20 and the RCMP silicon detector array'
+  },
+  {
+    student: 'Fatima-ezzahra Demyani',
+    institution: 'University of Grenoble-Alpes',
+    supervisor: 'P.E. Garrett',
+    year: '2024',
+    link: 'https://griffin.triumf.ca/theses.html',
+    linkTitle: 'Level Scheme Construction of 99Sr and Evaluation of Branching Ratios'
+  },
+  {
+    student: 'William Royer',
+    institution: 'University of Surrey/TRIUMF',
+    supervisor: 'A.B. Garnsworthy',
+    year: '2023',
+    link: 'https://griffin.triumf.ca',
+    linkTitle: 'Research Towards Commissioning ARIES, the New Ancillary Beta Tagging Array for Decay Spectroscopy at TRIUMF'
+  },
+  {
+    student: 'Nastaran Saei',
+    institution: 'University of Regina',
+    supervisor: 'G.F. Grinyer',
+    year: '2022',
+    link: 'https://uregina.scholaris.ca/items/9731666e-c15a-437c-83c4-659d40d99cb7',
+    linkTitle: 'Gamma-decay spectroscopy of sodium-26'
+  },
+  {
+    student: 'Robin Coleman',
+    institution: 'University of Guelph',
+    supervisor: 'C.E. Svensson',
+    year: '2020',
+    link: 'http://hdl.handle.net/10214/17744',
+    linkTitle: 'Beta Decay of Neutron-Rich 52K'
+  },
+  {
+    student: 'Charlotte Paxman',
+    institution: 'University of Surrey/TRIUMF',
+    supervisor: 'A.B. Garnsworthy',
+    year: '2020',
+    link: 'https://griffin.triumf.ca',
+    linkTitle: 'Internal conversion coefficients and spin assignments in Hg isotopes'
+  },
+  {
+    student: 'Joseph McAfee',
+    institution: 'University of Surrey/TRIUMF',
+    supervisor: 'A.B. Garnsworthy',
+    year: '2020',
+    link: 'https://griffin.triumf.ca',
+    linkTitle: 'Developing the Perturbed Angular Correlation spectroscopy (PACs) technique for the GRIFFIN spectrometer'
+  },
+  {
+    student: 'Daniel Yates',
+    institution: 'University of British Columbia/TRIUMF',
+    supervisor: 'R. Kruecken/I. Dillmann',
+    year: '2019',
+    link: 'http://hdl.handle.net/2429/71312',
+    linkTitle: 'Decay spectroscopy of Europium-160'
+  },
+  {
+    student: 'Max Winokan',
+    institution: 'University of Surrey/TRIUMF',
+    supervisor: 'A.B. Garnsworthy',
+    year: '2019',
+    link: 'https://griffin.triumf.ca/Theses/Surrey-MPhys-Winokan.pdf',
+    linkTitle: 'A new beta-scintillator array with optimized geometry for the GRIFFIN spectrometer'
+  },
+  {
+    student: 'Tammy Zidar',
+    institution: 'University of Guelph',
+    supervisor: 'C.E. Svensson',
+    year: '2018',
+    link: 'http://hdl.handle.net/10214/14706',
+    linkTitle: 'Beta decay of neutron-rich 33Mg'
+  },
+  {
+    student: 'Yukiya Saito',
+    institution: 'University of British Columbia/TRIUMF',
+    supervisor: 'R. Kruecken/I. Dillmann',
+    year: '2018',
+    link: 'https://open.library.ubc.ca/cIRcle/collections/ubctheses/24/items/1.0371206',
+    linkTitle: 'Decay spectroscopy of neutron-rich 129Cd with the GRIFFIN spectrometer'
+  },
+  {
+    student: 'Calum Jones',
+    institution: 'University of Surrey/TRIUMF',
+    supervisor: 'A.B. Garnsworthy',
+    year: '2018',
+    link: 'https://griffin.triumf.ca/Theses/Surrey-MPhys-Jones.pdf',
+    linkTitle: 'An Investigation of Branching Ratios, Comparative Half-Lives and Rotational Band Structure in 50Ti Nuclei'
+  },
+  {
+    student: 'James Measures',
+    institution: 'University of Surrey/TRIUMF',
+    supervisor: 'A.B. Garnsworthy',
+    year: '2017',
+    link: 'https://griffin.triumf.ca',
+    linkTitle: 'Structure of 26Mg Through the Analysis of the Beta Decay of 26Na'
+  },
+  {
+    student: 'Andrew MacLean',
+    institution: 'University of Guelph',
+    supervisor: 'C.E. Svensson',
+    year: '2016',
+    link: 'https://www.physics.uoguelph.ca/Nucweb/theses/AMacleanMsc.pdf',
+    linkTitle: 'Gamma-gamma angular correlation measurements with GRIFFIN'
+  },
+  {
+    student: 'Sam Hallam',
+    institution: 'University of Surrey/TRIUMF',
+    supervisor: 'A.B. Garnsworthy',
+    year: '2016',
+    link: 'https://griffin.triumf.ca/Theses/Surrey-MPhys-Hallam.pdf',
+    linkTitle: 'Designing CEDAR: Conversion Electron Detection ARray'
+  },
+  {
+    student: 'Usman Rizwan',
+    institution: 'Simon Fraser University',
+    supervisor: 'K. Starosta',
+    year: '2015',
+    link: 'http://summit.sfu.ca/item/15733',
+    linkTitle: 'Development of Gamma-Ray Spectroscopy Techniques for Fundamental and Applied Research'
+  },
+  {
+    student: 'Lisa Morrison',
+    institution: 'University of Surrey/TRIUMF',
+    supervisor: 'A.B. Garnsworthy',
+    year: '2015',
+    link: 'https://griffin.triumf.ca/Theses/Surrey-MPhys-Morrison.pdf',
+    linkTitle: 'Gamma-Gamma Angular Correlations with the GRIFFIN Spectrometer'
+  }
 ]
